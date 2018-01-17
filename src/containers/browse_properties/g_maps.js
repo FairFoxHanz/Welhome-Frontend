0656b023a03d06939ed1135c3b42f754208b9944
Piotr Ficek <piotrficek95@gmail.com>
2018-01-17 23:30:54 +0100

    Map is centered on poland

--- src/containers/browse_properties/g_maps.js
+++ src/containers/browse_properties/g_maps.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-const style = {
-    width: '100%',
-    height: '100%'
-  }
-
 export class MapContainer extends Component {
     constructor(props){
         super();
@@ -18,7 +13,7 @@ export class MapContainer extends Component {
     
 render() {
     return (
-      <Map google={this.props.google} zoom={2} className="card">
+      <Map google={this.props.google} zoom={5} className="card" disableDefaultUI center={{lat: 52, lng: 21 }} >
         <InfoWindow onClose={this.onInfoWindowClose}>
             <div>
               <h1>{this.state.selectedPlace.name}</h1>
